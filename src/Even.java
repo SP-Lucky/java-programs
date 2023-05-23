7b0adfaaa3df1c1b3b2c4d06f3f6755db8bff3b3
Lucky <splucky.456@gmail.com>
2023-05-23 15:48:06 +0530

    java programs

---
+++ src/Even.java
@@ -0,0 +1,20 @@
+
+public class Even {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=20;i++)
+		{
+			if(i%2==0)
+			{
+			System.out.println("Even numbers"+i);
+			}
+			if(i%2==1)
+			{
+				System.out.println("odd"+i);
+			}
+		}
+
+	}
+
+}
---
+++ src/Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int i=1,fact=1;
+		for(i=5;i>=5;i--)
+		{
+			fact=fact*1;
+			
+		}
+			System.out.println("factorial  "+fact);
+	}
+
+}
